bfdd520ccb5eca33dfa90536eb3ae42dab24834c
Kushagra Choudhary <kushagra26407@gmail.com>
2025-09-24 20:44:37 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/cogni-canvas/excalidraw-wrapper.tsx
+++ src/components/cogni-canvas/excalidraw-wrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useEffect, useState, memo } from "react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 import type { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 import type { AppState } from "@excalidraw/excalidraw/types/types";
 
@@ -17,40 +18,32 @@ const ExcalidrawWrapper = ({
   initialElements,
   onChange,
 }: ExcalidrawWrapperProps) => {
-  const [Excalidraw, setExcalidraw] = useState<any>(null);
-
-  useEffect(() => {
-    import("@excalidraw/excalidraw").then((comp) =>
-      setExcalidraw(comp.Excalidraw)
-    );
-  }, []);
+  const Excalidraw = useMemo(
+    () =>
+      dynamic(() => import("@excalidraw/excalidraw").then((mod) => mod.Excalidraw), {
+        ssr: false,
+        loading: () => (
+          <div className="flex h-full w-full items-center justify-center">
+            <p>Loading Canvas...</p>
+          </div>
+        ),
+      }),
+    []
+  );
 
   return (
     <div className="h-full w-full">
-      {Excalidraw ? (
-        (() => {
-          // We need to wrap the Excalidraw component in a memo to prevent
-          // re-rendering on every prop change, which would be expensive.
-          const MemoizedExcalidraw = memo(Excalidraw);
-          return (
-            <MemoizedExcalidraw
-              key={JSON.stringify(initialElements)}
-              initialData={{
-                elements: initialElements,
-                appState: {
-                  viewBackgroundColor: "#141414",
-                  currentItemStrokeColor: "#aca7f2",
-                },
-              }}
-              onChange={onChange}
-            />
-          );
-        })()
-      ) : (
-        <div className="flex h-full w-full items-center justify-center">
-          <p>Loading Canvas...</p>
-        </div>
-      )}
+      <Excalidraw
+        key={JSON.stringify(initialElements)}
+        initialData={{
+          elements: initialElements,
+          appState: {
+            viewBackgroundColor: "#141414",
+            currentItemStrokeColor: "#aca7f2",
+          },
+        }}
+        onChange={onChange}
+      />
     </div>
   );
 };
