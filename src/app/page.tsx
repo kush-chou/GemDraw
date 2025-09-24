a6c268cc42831abcbb961b5b714c185b14acaa32
Kushagra Choudhary <kushagra26407@gmail.com>
2025-09-24 20:31:59 +0000

    Put the chatbox on the left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import ExcalidrawWrapper from "@/components/cogni-canvas/excalidraw-wrapper";
 export default function Home() {
   return (
     <main className="flex h-screen w-full bg-background">
+      <aside className="w-96 flex flex-col bg-card border-r border-border">
+        <ChatPanel />
+      </aside>
       <div className="flex-grow h-full">
         <ExcalidrawWrapper />
       </div>
-      <aside className="w-96 flex flex-col bg-card border-l border-border">
-        <ChatPanel />
-      </aside>
     </main>
   );
 }
